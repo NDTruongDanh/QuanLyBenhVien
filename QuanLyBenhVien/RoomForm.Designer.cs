8dc3c9ec9f50ac184a72dcadde649456df4df6fb
23520187 <23520187@gm.uit.edu.vn>
2024-12-01 08:21:05 +0700

    đổi tên control của Room

--- QuanLyBenhVien/RoomForm.Designer.cs
+++ QuanLyBenhVien/RoomForm.Designer.cs
@@ -34,13 +34,15 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtRoomID = new System.Windows.Forms.TextBox();
+            this.txtUsageBed = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnRemoveRoom = new System.Windows.Forms.Button();
+            this.btnAddOrUpdateRoom = new System.Windows.Forms.Button();
+            this.btnRefreshRoom = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbDepartmentID = new System.Windows.Forms.ComboBox();
+            this.cmbRoomType = new System.Windows.Forms.ComboBox();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.tableLayoutPanel2.SuspendLayout();
@@ -83,12 +85,15 @@
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
             this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.tableLayoutPanel2.Controls.Add(this.label1, 0, 0);
             this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
             this.tableLayoutPanel2.Controls.Add(this.label4, 2, 1);
-            this.tableLayoutPanel2.Controls.Add(this.textBox1, 1, 0);
-            this.tableLayoutPanel2.Controls.Add(this.textBox2, 3, 1);
-            this.tableLayoutPanel2.Controls.Add(this.textBox3, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.txtRoomID, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.txtUsageBed, 3, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label2, 2, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cmbDepartmentID, 3, 0);
+            this.tableLayoutPanel2.Controls.Add(this.cmbRoomType, 1, 1);
             this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel2.Location = new System.Drawing.Point(4, 4);
             this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -107,7 +112,7 @@
             this.label1.Location = new System.Drawing.Point(4, 0);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 92);
+            this.label1.Size = new System.Drawing.Size(119, 92);
             this.label1.TabIndex = 0;
             this.label1.Text = "Mã phòng";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -120,9 +125,9 @@
             this.label3.Location = new System.Drawing.Point(4, 92);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(110, 92);
+            this.label3.Size = new System.Drawing.Size(119, 92);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Số giường";
+            this.label3.Text = "Loại phòng";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label4
@@ -130,43 +135,33 @@
             this.label4.AutoSize = true;
             this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(506, 92);
+            this.label4.Location = new System.Drawing.Point(493, 92);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(165, 92);
+            this.label4.Size = new System.Drawing.Size(200, 92);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Số giường trống";
+            this.label4.Text = "Giường có thể dùng";
             this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // textBox1
+            // txtRoomID
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(122, 31);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(376, 30);
-            this.textBox1.TabIndex = 4;
+            this.txtRoomID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRoomID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRoomID.Location = new System.Drawing.Point(131, 31);
+            this.txtRoomID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtRoomID.Name = "txtRoomID";
+            this.txtRoomID.Size = new System.Drawing.Size(354, 30);
+            this.txtRoomID.TabIndex = 4;
             // 
-            // textBox2
+            // txtUsageBed
             // 
-            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(679, 123);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(376, 30);
-            this.textBox2.TabIndex = 5;
-            // 
-            // textBox3
-            // 
-            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(122, 123);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(376, 30);
-            this.textBox3.TabIndex = 6;
+            this.txtUsageBed.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUsageBed.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsageBed.Location = new System.Drawing.Point(701, 123);
+            this.txtUsageBed.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUsageBed.Name = "txtUsageBed";
+            this.txtUsageBed.Size = new System.Drawing.Size(354, 30);
+            this.txtUsageBed.TabIndex = 5;
             // 
             // tableLayoutPanel3
             // 
@@ -174,9 +169,9 @@
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel3.Controls.Add(this.button3, 2, 0);
-            this.tableLayoutPanel3.Controls.Add(this.button2, 1, 0);
-            this.tableLayoutPanel3.Controls.Add(this.button1, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnRemoveRoom, 2, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnAddOrUpdateRoom, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnRefreshRoom, 0, 0);
             this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel3.Location = new System.Drawing.Point(4, 196);
             this.tableLayoutPanel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
@@ -186,41 +181,73 @@
             this.tableLayoutPanel3.Size = new System.Drawing.Size(1059, 40);
             this.tableLayoutPanel3.TabIndex = 2;
             // 
-            // button3
+            // btnRemoveRoom
             // 
-            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(710, 4);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(345, 32);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Xóa";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnRemoveRoom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRemoveRoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoveRoom.Location = new System.Drawing.Point(710, 4);
+            this.btnRemoveRoom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRemoveRoom.Name = "btnRemoveRoom";
+            this.btnRemoveRoom.Size = new System.Drawing.Size(345, 32);
+            this.btnRemoveRoom.TabIndex = 2;
+            this.btnRemoveRoom.Text = "Xóa";
+            this.btnRemoveRoom.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnAddOrUpdateRoom
             // 
-            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(357, 4);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(345, 32);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Thêm/Sửa";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnAddOrUpdateRoom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAddOrUpdateRoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddOrUpdateRoom.Location = new System.Drawing.Point(357, 4);
+            this.btnAddOrUpdateRoom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAddOrUpdateRoom.Name = "btnAddOrUpdateRoom";
+            this.btnAddOrUpdateRoom.Size = new System.Drawing.Size(345, 32);
+            this.btnAddOrUpdateRoom.TabIndex = 1;
+            this.btnAddOrUpdateRoom.Text = "Thêm/Sửa";
+            this.btnAddOrUpdateRoom.UseVisualStyleBackColor = true;
             // 
-            // button1
+            // btnRefreshRoom
             // 
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(4, 4);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(345, 32);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Làm mới";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnRefreshRoom.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnRefreshRoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRefreshRoom.Location = new System.Drawing.Point(4, 4);
+            this.btnRefreshRoom.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRefreshRoom.Name = "btnRefreshRoom";
+            this.btnRefreshRoom.Size = new System.Drawing.Size(345, 32);
+            this.btnRefreshRoom.TabIndex = 0;
+            this.btnRefreshRoom.Text = "Làm mới";
+            this.btnRefreshRoom.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(492, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Mã khoa";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // cmbDepartmentID
+            // 
+            this.cmbDepartmentID.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbDepartmentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDepartmentID.FormattingEnabled = true;
+            this.cmbDepartmentID.Location = new System.Drawing.Point(700, 34);
+            this.cmbDepartmentID.Name = "cmbDepartmentID";
+            this.cmbDepartmentID.Size = new System.Drawing.Size(356, 33);
+            this.cmbDepartmentID.TabIndex = 8;
+            // 
+            // cmbRoomType
+            // 
+            this.cmbRoomType.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbRoomType.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbRoomType.FormattingEnabled = true;
+            this.cmbRoomType.Location = new System.Drawing.Point(130, 126);
+            this.cmbRoomType.Name = "cmbRoomType";
+            this.cmbRoomType.Size = new System.Drawing.Size(356, 33);
+            this.cmbRoomType.TabIndex = 9;
             // 
             // RoomForm
             // 
@@ -248,12 +275,14 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtRoomID;
+        private System.Windows.Forms.TextBox txtUsageBed;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnRemoveRoom;
+        private System.Windows.Forms.Button btnAddOrUpdateRoom;
+        private System.Windows.Forms.Button btnRefreshRoom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbDepartmentID;
+        private System.Windows.Forms.ComboBox cmbRoomType;
     }
 }
