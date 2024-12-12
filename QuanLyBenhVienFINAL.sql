3354b2d46f4337ab4cd501f5171882aa3c5ed87b
luubinhdeptrai <luubinh.chuyenlongan@gmail.com>
2024-12-12 20:24:36 +0700

    SuaDB

--- QuanLyBenhVienFINAL.sql
+++ QuanLyBenhVienFINAL.sql
@@ -4,7 +4,7 @@ CREATE DATABASE HospitalDB
 
 USE HospitalDB
 
-SET DATEFORMAT DMY  --Thiết lập DATE theo thứ tự ngày->tháng->năm
+SET DATEFORMAT YMD  --Thiết lập DATE theo thứ tự ngày->tháng->năm
 
 --Thống nhất tất cả các ID đều có đúng 6 ký tự bao gồm 2 chữ 4 số--
 CREATE TABLE PATIENT (
@@ -77,6 +77,7 @@ CREATE TABLE BILLDETAIL (
     TransactionID CHAR(6),
     MedicationID CHAR(6),
     Amount INT,  -- Số lượng thuốc
+	TotelOfDetail MONEY,
     PRIMARY KEY (TransactionID, MedicationID)
 )
 
@@ -155,4 +156,107 @@ ADD CONSTRAINT FK_WE_STA FOREIGN KEY (StaffID) REFERENCES STAFF(StaffID)
 
 ALTER TABLE WEEKLYASSIGNMENT
 ADD CONSTRAINT FK_WE_DE FOREIGN KEY (DepartmentID) REFERENCES DEPARTMENT(DepartmentID) 
+
+
+
+
+-- Chèn dữ liệu vào bảng PATIENT
+INSERT INTO PATIENT (PatientID, FullName, DateOfBirth, Gender, PhoneNumber, AddressPatient, Email, AdmissionDate, DischargeDate, RoomID)
+VALUES 
+('PA0001', N'Nguyễn Văn A', '1990-05-01', N'Nam', '0901234567', N'Hà Nội', 'a.nguyen@example.com', '2024-12-01', '2024-12-05', 'RO0001'),
+('PA0002', N'Phạm Thị B', '1985-10-15', N'Nữ', '0902345678', N'Đà Nẵng', 'b.pham@example.com', '2024-12-02', '2024-12-06', 'RO0002'),
+('PA0003', N'Ngô Văn C', '2000-07-20', N'Nam', '0903456789', N'Hồ Chí Minh', 'c.ngo@example.com', '2024-12-03', '2024-12-07', 'RO0003'),
+('PA0004', N'Trần Thị D', '1992-02-10', N'Nữ', '0904567890', N'Quảng Ninh', 'd.tran@example.com', '2024-12-04', '2024-12-08', 'RO0004'),
+('PA0005', N'Lê Minh E', '1980-11-11', N'Nam', '0905678901', N'Bình Dương', 'e.le@example.com', '2024-12-05', '2024-12-09', 'RO0005'),
+('PA0006', N'Vũ Thị F', '1995-03-22', N'Nữ', '0906789012', N'Vũng Tàu', 'f.vu@example.com', '2024-12-06', '2024-12-10', 'RO0006');
+
+-- Chèn dữ liệu vào bảng STAFF
+INSERT INTO STAFF (StaffID, FullName, TypeOfStaff, Gender, DateOfBirth, PhoneNumber, DateOfJoining, Email, Salary, DepartmentID)
+VALUES
+('ST0001', N'Nguyễn Thiện G', N'Bác sĩ Đa khoa', N'Nam', '1980-01-15', '0912345678', '2020-05-01', 'g.nguyen@example.com', 15000000, 'DP0001'),
+('ST0002', N'Phạm Minh H', N'Kế toán', N'Nữ', '1990-06-20', '0913456789', '2022-07-01', 'h.pham@example.com', 10000000, 'DP0002'),
+('ST0003', N'Lê Thị I', N'Quản lý', N'Nữ', '1985-09-30', '0914567890', '2018-08-15', 'i.le@example.com', 20000000, 'DP0003'),
+('ST0004', N'Vũ Thái J', N'Kế toán', N'Nam', '1978-12-25', '0915678901', '2015-11-20', 'j.vu@example.com', 18000000, 'DP0004'),
+('ST0005', N'Trần Minh K', N'Điều dưỡng Tổng quát', N'Nam', '1992-05-05', '0916789012', '2023-09-10', 'k.tran@example.com', 11000000, 'DP0005'),
+('ST0006', N'Võ Quang L', N'Bác sĩ Ngoại khoa', N'Nam', '1982-03-11', '0917890123', '2019-06-01', 'l.vo@example.com', 17000000, 'DP0006'),
+('ST0007', N'Võ Quang A', N'Bác sĩ Thần kinh', N'Nam', '1983-03-14', '0917890127', '2020-06-02', 'a.vo@example.com', 28000000, 'DP0001'),
+('ST0008', N'Võ Quang B', N'Bác sĩ Ung bứu', N'Nam', '1985-03-11', '0917890121', '2018-07-01', 'b.vo@example.com', 17500000, 'DP0002');
+
+-- Chèn dữ liệu vào bảng DEPARTMENT
+INSERT INTO DEPARTMENT (DepartmentID, DepartmentName, EmployeeNumber, HeadDepartmentID, PhoneNumber, LocationDPM)
+VALUES
+('DP0001', N'Khoa Nội', 2, 'ST0001', '0241234567', N'Hà Nội'),
+('DP0002', N'Khoa Ngoại', 2, 'ST0002', '0242345678', N'Đà Nẵng'),
+('DP0003', N'Khoa Cấp Cứu', 1, 'ST0003', '0243456789', N'Hồ Chí Minh'),
+('DP0004', N'Khoa Nhi', 1, 'ST0004', '0244567890', N'Quảng Ninh'),
+('DP0005', N'Khoa Sản', 1, 'ST0005', '0245678901', N'Bình Dương'),
+('DP0006', N'Khoa Tim Mạch', 1, 'ST0006', '0246789012', N'Vũng Tàu');
+
+-- Chèn dữ liệu vào bảng APPOINTMENT
+INSERT INTO APPOINTMENT (AppointmentID, PatientID, DoctorID, DepartmentID, AppointmentDateTime, AppointmentStatus)
+VALUES
+('AP0001', 'PA0001', 'ST0001', 'DP0001', '2024-12-01 08:00:00', N'Hoàn thành'),
+('AP0002', 'PA0002', 'ST0006', 'DP0002', '2024-12-02 09:00:00', N'Chờ xác nhận'),
+('AP0003', 'PA0003', 'ST0007', 'DP0003', '2024-12-03 10:00:00', N'Hoàn thành'),
+('AP0004', 'PA0004', 'ST0008', 'DP0004', '2024-12-04 11:00:00', N'Hoàn thành'),
+('AP0005', 'PA0005', 'ST0001', 'DP0005', '2024-12-05 12:00:00', N'Hủy'),
+('AP0006', 'PA0006', 'ST0006', 'DP0006', '2024-12-06 13:00:00', N'Hoàn thành');
+
+-- Chèn dữ liệu vào bảng MEDICALRECORD
+INSERT INTO MEDICALRECORD (RecordID, PatientID, DoctorID, VisitDate, Diagnosis, TestResults, TreatmentPlan)
+VALUES
+('MR0001', 'PA0001', 'ST0001', '2024-12-01', N'Viêm phổi', N'X-ray bình thường', N'Điều trị kháng sinh'),
+('MR0002', 'PA0003', 'ST0007', '2024-12-03', N'Đau dạ dày', N'Siêu âm bình thường', N'Thuốc giảm đau'),
+('MR0003', 'PA0004', 'ST0008', '2024-12-04', N'Cảm cúm', N'Xét nghiệm máu', N'Thức ăn nhẹ, uống nước nhiều'),
+('MR0004', 'PA0006', 'ST0006', '2024-12-06', N'Bệnh tim mạch', N'ECG bình thường', N'Thuốc tim mạch')
+
+-- Chèn dữ liệu vào bảng BILL
+INSERT INTO BILL (TransactionID, RecordID, StaffID, TransactionDate, PaymentMethod, Total)
+VALUES
+('BI0001', 'MR0001', 'ST0004', '2024-12-01', N'Tiền mặt', 340000),
+('BI0002', 'MR0002', 'ST0002', '2024-12-03', N'Thẻ tín dụng', 120000),
+('BI0003', 'MR0003', 'ST0002', '2024-12-04', N'Bảo hiểm', 380000),
+('BI0004', 'MR0004', 'ST0004', '2024-12-06', N'Tiền mặt', 280000)
+
+-- Chèn dữ liệu vào bảng BILLDETAIL
+INSERT INTO BILLDETAIL (TransactionID, MedicationID, Amount, TotelOfDetail)
+VALUES
+('BI0001', 'ME0001', 2, 100000),
+('BI0001', 'ME0002', 3, 240000),
+('BI0002', 'ME0003', 1, 120000),
+('BI0003', 'ME0004', 5, 300000),
+('BI0003', 'ME0005', 2, 80000),
+('BI0004', 'ME0006', 4, 280000);
+
+-- Chèn dữ liệu vào bảng MEDICATION
+INSERT INTO MEDICATION (MedicationID, MedicationName, Dosage, Category, QuantityInStock, Price, ExpiryDate, ManufacturingDate, Manufacturer)
+VALUES
+('ME0001', N'Paracetamol', N'500mg', N'Giảm đau', 100, 50000, '2025-12-01', '2024-01-01', N'Việt Nam'),
+('ME0002', N'Amoxicillin', N'250mg', N'Kháng sinh', 50, 80000, '2025-06-01', '2024-02-01', N'Ấn Độ'),
+('ME0003', N'Ciprofloxacin', N'500mg', N'Kháng sinh', 70, 120000, '2025-09-01', '2024-03-01', N'Mỹ'),
+('ME0004', N'Ibuprofen', N'400mg', N'Giảm đau', 150, 60000, '2025-07-01', '2024-04-01', N'Anh'),
+('ME0005', N'Omeprazole', N'20mg', N'Tiêu hóa', 200, 40000, '2025-11-01', '2024-05-01', N'Nhật Bản'),
+('ME0006', N'Diclofenac', N'50mg', N'Giảm đau', 120, 70000, '2025-08-01', '2024-06-01', N'Trung Quốc');
+
+
+-- Chèn dữ liệu vào bảng ROOM
+INSERT INTO ROOM (RoomID, DepartmentID, BedCount, RoomType)
+VALUES
+('RO0001', 'DP0001', 10, N'Điều trị tổng quát'),
+('RO0002', 'DP0002', 12, N'VIP'),
+('RO0003', 'DP0003', 8, N'Hồi sức'),
+('RO0004', 'DP0004', 6, N'Điều trị tổng quát'),
+('RO0005', 'DP0005', 4, N'VIP'),
+('RO0006', 'DP0006', 5, N'Sản khoa');
+
+
+-- Chèn dữ liệu vào bảng WEEKLYASSIGNMENT
+INSERT INTO WEEKLYASSIGNMENT (AssignmentID, StaffID, DepartmentID, WeekStartDate, WeekEndDate, ShiftType)
+VALUES
+('WA0001', 'ST0001', 'DP0001', '2024-12-01', '2024-12-07', N'Sáng'),
+('WA0002', 'ST0002', 'DP0002', '2024-12-01', '2024-12-07', N'Chiều'),
+('WA0003', 'ST0003', 'DP0003', '2024-12-01', '2024-12-07', N'Tối'),
+('WA0004', 'ST0004', 'DP0004', '2024-12-01', '2024-12-07', N'Sáng'),
+('WA0005', 'ST0005', 'DP0005', '2024-12-01', '2024-12-07', N'Chiều'),
+('WA0006', 'ST0006', 'DP0006', '2024-12-01', '2024-12-07', N'Tối');
 
