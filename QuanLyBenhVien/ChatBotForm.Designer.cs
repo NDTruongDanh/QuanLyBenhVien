0b822f8366c33e9d22ac3f5f2821dd280bd4b791
NDTruongDanh <23520242@gm.uit.edu.vn>
2024-12-13 18:39:39 +0700

    Thay đổi giao diện chatbot

--- QuanLyBenhVien/ChatBotForm.Designer.cs
+++ QuanLyBenhVien/ChatBotForm.Designer.cs
@@ -30,8 +30,8 @@
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.txtPrompt = new System.Windows.Forms.TextBox();
-            this.txtResponse = new System.Windows.Forms.TextBox();
             this.btnGenerate = new System.Windows.Forms.Button();
+            this.txtChat = new System.Windows.Forms.RichTextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -39,58 +39,61 @@
             // 
             this.tableLayoutPanel1.ColumnCount = 1;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.txtResponse, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.txtPrompt, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.btnGenerate, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtPrompt, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnGenerate, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtChat, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 60F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 475);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // txtPrompt
             // 
             this.txtPrompt.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txtPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPrompt.Location = new System.Drawing.Point(3, 3);
+            this.txtPrompt.Location = new System.Drawing.Point(3, 288);
             this.txtPrompt.Multiline = true;
             this.txtPrompt.Name = "txtPrompt";
-            this.txtPrompt.Size = new System.Drawing.Size(794, 174);
-            this.txtPrompt.TabIndex = 0;
-            // 
-            // txtResponse
-            // 
-            this.txtResponse.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.txtResponse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtResponse.Location = new System.Drawing.Point(3, 228);
-            this.txtResponse.Multiline = true;
-            this.txtResponse.Name = "txtResponse";
-            this.txtResponse.ReadOnly = true;
-            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.txtResponse.Size = new System.Drawing.Size(794, 219);
-            this.txtResponse.TabIndex = 2;
+            this.txtPrompt.Size = new System.Drawing.Size(794, 136);
+            this.txtPrompt.TabIndex = 1;
             // 
             // btnGenerate
             // 
-            this.btnGenerate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGenerate.AutoSize = true;
             this.btnGenerate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnGenerate.Location = new System.Drawing.Point(3, 183);
+            this.btnGenerate.Location = new System.Drawing.Point(722, 430);
             this.btnGenerate.Name = "btnGenerate";
-            this.btnGenerate.Size = new System.Drawing.Size(794, 39);
-            this.btnGenerate.TabIndex = 3;
-            this.btnGenerate.Text = "Nhập";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 42);
+            this.btnGenerate.TabIndex = 2;
+            this.btnGenerate.Text = "Hỏi";
             this.btnGenerate.UseVisualStyleBackColor = true;
             this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // txtChat
+            // 
+            this.txtChat.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtChat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtChat.Location = new System.Drawing.Point(3, 3);
+            this.txtChat.Name = "txtChat";
+            this.txtChat.ReadOnly = true;
+            this.txtChat.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.txtChat.Size = new System.Drawing.Size(794, 279);
+            this.txtChat.TabIndex = 3;
+            this.txtChat.Text = "";
             // 
             // ChatBotForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 475);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "ChatBotForm";
             this.Text = "TRỢ LÝ ẢO";
@@ -103,8 +106,8 @@
         #endregion
 
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.TextBox txtResponse;
         private System.Windows.Forms.TextBox txtPrompt;
         private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.RichTextBox txtChat;
     }
 }
