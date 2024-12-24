8b56adc77cf5bb6258255faacf84c43616996618
23520187 <23520187@gm.uit.edu.vn>
2024-12-24 20:59:27 +0700

    Fix thuoc ton kho

--- QuanLyBenhVien/EmployeeForm.Designer.cs
+++ QuanLyBenhVien/EmployeeForm.Designer.cs
@@ -163,7 +163,7 @@
             "Giảng viên Y khoa",
             "Kế toán",
             "Quản lý"});
-            this.cmbTypeOfStaff.Location = new System.Drawing.Point(687, 93);
+            this.cmbTypeOfStaff.Location = new System.Drawing.Point(687, 88);
             this.cmbTypeOfStaff.Name = "cmbTypeOfStaff";
             this.cmbTypeOfStaff.Size = new System.Drawing.Size(369, 33);
             this.cmbTypeOfStaff.TabIndex = 26;
@@ -334,6 +334,7 @@
             // 
             this.dtpDateOfJoining.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
             this.dtpDateOfJoining.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpDateOfJoining.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dtpDateOfJoining.Location = new System.Drawing.Point(155, 175);
             this.dtpDateOfJoining.Margin = new System.Windows.Forms.Padding(4);
             this.dtpDateOfJoining.Name = "dtpDateOfJoining";
@@ -372,7 +373,7 @@
             this.cmbDepartmentID.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
             this.cmbDepartmentID.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
             this.cmbDepartmentID.FormattingEnabled = true;
-            this.cmbDepartmentID.Location = new System.Drawing.Point(154, 93);
+            this.cmbDepartmentID.Location = new System.Drawing.Point(154, 88);
             this.cmbDepartmentID.Name = "cmbDepartmentID";
             this.cmbDepartmentID.Size = new System.Drawing.Size(369, 33);
             this.cmbDepartmentID.TabIndex = 25;
